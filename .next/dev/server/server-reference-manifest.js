cbd7929419bccc8cd22c9440f1a9facc30ffe8c2
zengmi <48661436+zengmi2140@users.noreply.github.com>
2025-12-20 12:41:41 +0000

    Remove files that should be ignored

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"d+xCe+kGyW+n4jWFRn2pwx8ts3OBgfE26t/UqaEbjDs=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__6d65459c._.css",
-  "static/chunks/node_modules_901137b7._.js"
-],
-    source: "dynamic"
-});
